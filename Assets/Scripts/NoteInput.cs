d91c6a6f53a93e7fea3d7819d3d9de605ea26de9
Utz Stauder <utz.stauder@BK.local>
2019-08-13 10:39:23 +0200

    Initial commit

---
+++ Assets/Scripts/NoteInput.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteInput : MonoBehaviour
+{
+    public delegate void NoteOnDelegate(int noteNumber, float velocity);
+    public static event NoteOnDelegate NoteOn;
+
+    public delegate void NoteOffDelegate(int noteNumber);
+    public static event NoteOffDelegate NoteOff;
+
+    Dictionary<KeyCode, int> virtualKeysDict;
+
+    private void Awake()
+    {
+        virtualKeysDict = new Dictionary<KeyCode, int>();
+        virtualKeysDict.Add(KeyCode.A, 51); // C
+        virtualKeysDict.Add(KeyCode.W, 52); // C#
+        virtualKeysDict.Add(KeyCode.S, 53); // D
+        virtualKeysDict.Add(KeyCode.E, 54); // D#
+        virtualKeysDict.Add(KeyCode.D, 55); // E
+        virtualKeysDict.Add(KeyCode.F, 56); // F
+        virtualKeysDict.Add(KeyCode.T, 57); // F#
+        virtualKeysDict.Add(KeyCode.G, 58); // G
+        virtualKeysDict.Add(KeyCode.Z, 59); // G#
+        virtualKeysDict.Add(KeyCode.H, 60); // A
+        virtualKeysDict.Add(KeyCode.U, 61); // A#
+        virtualKeysDict.Add(KeyCode.J, 62); // B
+        virtualKeysDict.Add(KeyCode.K, 63); // C
+
+        // ...
+    }
+
+    private void Update()
+    {
+        foreach (var key in virtualKeysDict.Keys)
+        {
+            if (Input.GetKeyDown(key))
+            {
+                NoteOn?.Invoke(virtualKeysDict[key], 1f);
+            }
+
+            if (Input.GetKeyUp(key))
+            {
+                NoteOff?.Invoke(virtualKeysDict[key]);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Synthesizer.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Synthesizer : MonoBehaviour
+{
+    private System.Random random = new System.Random();
+
+    private float tick = 0;
+
+    float sampleRate;
+    double increment;
+    double phase;
+
+    [Header("Audio Controls")]
+    [SerializeField, Range(0, 1)] float volume = 0.5f;
+    [SerializeField] float frequency = 440f;
+
+    public enum WaveType
+    {
+        Noise,
+        Sine
+    }
+
+    [SerializeField] private WaveType waveType;
+
+
+    #region Unity Messages
+
+    private void OnEnable()
+    {
+        sampleRate = (float)AudioSettings.outputSampleRate;
+        Debug.LogFormat("Output Sample Rate = {0}", sampleRate);
+
+        NoteInput.NoteOn += NoteInput_NoteOn;
+        NoteInput.NoteOff += NoteInput_NoteOff;
+    }
+
+    private void OnDisable()
+    {
+        NoteInput.NoteOn -= NoteInput_NoteOn;
+        NoteInput.NoteOff -= NoteInput_NoteOff;
+    }
+
+    private void OnAudioFilterRead(float[] data, int channels)
+    {
+        increment = frequency * 2 * Mathf.PI / sampleRate;
+
+        // clear audio buffer
+        for (int i = 0; i < data.Length; i++)
+        {
+            data[i] = 0;
+        }
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            phase += increment;
+
+            switch (waveType)
+            {
+                case WaveType.Noise:
+                    {
+                        // generate white noise
+                        data[i] = (float)(random.NextDouble() * 2 - 1) * volume;
+                    }
+                    break;
+                case WaveType.Sine:
+                    data[i] = Mathf.Sin((float)phase) * volume;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    #endregion
+
+
+    #region Event Handler
+
+    private void NoteInput_NoteOn(int noteNumber, float velocity)
+    {
+        Debug.LogFormat("NoteOn: {0}", noteNumber);
+        frequency = NoteToFrequency(noteNumber);
+    }
+
+    private void NoteInput_NoteOff(int noteNumber)
+    {
+        Debug.LogFormat("NoteOff: {0}", noteNumber);
+    }
+
+    #endregion
+
+    // A4 = 440Hz = noteNumber 60
+    public static float NoteToFrequency(int noteNumber)
+    {
+        float twelfthRoot = Mathf.Pow(2f, (1f / 12f));
+        int fixedNoteNumber = 60;
+        return 440f * Mathf.Pow(twelfthRoot, (noteNumber - fixedNoteNumber));
+    }
+}
